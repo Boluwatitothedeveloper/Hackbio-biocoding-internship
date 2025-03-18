d302066159092948eaeda027e2826648c39c76d1
Boluwatitothedeveloper <105285033+Boluwatitothedeveloper@users.noreply.github.com>
2025-03-18 12:21:06 +0100

    Add files via upload

---
+++ Stage zero/hack.py
@@ -0,0 +1,60 @@
+print("Hello,world! Welcome to Hackbio")
+print("This is team Glutamine, and below is our group members details")
+team_members ={
+    "A":{
+        "Name": "Ajayi-Segun Flourish",
+        "Slack_username": "Titobliss",
+        "Email": "flourishajayisegun@gmail.com",
+        "Hobby": "listening to music," "writing," "reading.",
+        "Country": "Nigeria",
+        "Discipline": "Cell biology and genetics",
+        "preferred programming language": "python",
+},
+    "B": {"Name": "Ajanah Blessing",
+        "Slack_username":"blessing",
+        "Email": "ajanahblessing@gmail.com",
+        "Hobby": "listening to music," "learning new things.",
+        "Country": "Nigeria",
+        "Discipline":"Cell biology and genetics",
+        "preferred programming language": "python",
+
+ },
+   "C":{
+       "Name": "Adedeji Jesulayomi",
+       "Slack_username": "Jesulayomi",
+       "Email": "adedejitolu724@gmail.com",
+       "Hobby": "watching educative content",
+       "Discipline" : "Cell biology and genetics",
+       "preferred programming language": "python",
+}
+}
+
+#Displaying members details
+print("\nTeam Members Details:")
+
+#Member A
+print("\nMember A:")
+print("Name:", team_members["A"]["Name"])
+print("slack username:", team_members["A"]["Slack_username"])
+print("Email:", team_members["A"]["Email"])
+print("Hobby:", team_members["A"]["Hobby"])
+print("Dicipline:", team_members["A"]["Discipline"])
+print("Preferred programming language:", team_members["A"]["preferred programming language"])
+
+#Member B
+print("\nMember B:")
+print("Name:", team_members["B"]["Name"])
+print("slack username:", team_members["B"]["Slack_username"])
+print("Email:", team_members["B"]["Email"])
+print("Hobby:", team_members["B"]["Hobby"])
+print("Dicipline:", team_members["B"]["Discipline"])
+print("Preferred programming language:", team_members["B"]["preferred programming language"])
+
+#Member C
+print("\nMember C:")
+print("Name:", team_members["C"]["Name"])
+print("slack username:", team_members["C"]["Slack_username"])
+print("Email:", team_members["C"]["Email"])
+print("Hobby:", team_members["C"]["Hobby"])
+print("Dicipline:", team_members["C"]["Discipline"])
+print("Preferred programming language:", team_members["C"]["preferred programming language"])
